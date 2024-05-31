944881ced5fdf05119b54c336c8e73c0abb2d000
Marbletm <noahtekent@gmail.com>
2024-06-01 00:02:38 +0200

    Random update

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local query = get("query");
 local btn = get("search-button")
+local rndmbtn = get("random-button")
 
 local items = get("item", true);
 local itemName = get("item-name", true);
@@ -50,4 +51,27 @@ query.on_submit(queryServer);
 
 btn.on_click(function()
     queryServer(query.get_content())
+end)
+
+rndmbtn.on_click(function()
+    for index, item in pairs(items) do
+        item.set_opacity(0);
+    end
+
+    local field = items[1];
+    local itemEl = items[1];
+    local nameEl = itemName[1];
+    local ipEl = itemIp[1];
+    local urlEl = itemUrl[1];
+
+    field.set_opacity(1);
+
+    local item = response[math.random(#response)];
+    local url = "buss://" .. item["name"] .. "." .. item["tld"];
+
+    itemEl.set_opacity(1);
+    nameEl.set_content(item["name"]);
+    ipEl.set_content(item["ip"]);
+    urlEl.set_content(url);
+    urlEl.set_href(url);
 end)
