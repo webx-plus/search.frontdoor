e0def61f258080f675f3771562eecff260b9fa48
Marbletm <noahtekent@gmail.com>
2024-06-03 01:58:39 +0200

    webxplussearch 2.0

--- main.lua
+++ main.lua
@@ -1,77 +1,169 @@
-local query = get("query");
+local query = get(".query");
 local btn = get("search-button")
 local rndmbtn = get("random-button")
-
 local items = get("item", true);
 local itemName = get("item-name", true);
 local itemUrl = get("item-url", true);
 local itemIp = get("item-ip", true);
+local nextbtn = get('next-button');
+local previousbtn = get('previous-button');
+local currentPage = get('cur-page');
+local totalPage = get('total-page');
 
-for index, item in pairs(items) do
-    item.set_opacity(0);
+local page = 0;
+local totalPages = 1;
+local limit = 300;
+local queried = "";
+local dns = "https://bussin-dns.votemanager.xyz/api/domains/";
+
+function main()
+    clearItems();
 end
 
-local response = fetch({
-    url = "https://bussin-dns.votemanager.xyz/",
-    method = "GET",
-    headers = { },
-    body = ""
-});
+-- Declare functions
+------------------------------------
+-- Get URL of an item
+function getURL(item)
+    return (item["name"] .. "." .. item["tld"]);
+end
 
-function queryServer(content)
-    for index, item in pairs(items) do
-        item.set_opacity(0);
+-- Convert character to hexadecimal
+local char_to_hex = function(c)
+    return string.format("%%%02X", string.byte(c))
+end
+
+-- URL encode the given string
+local function urlencode(url)
+    if url == nil then
+        return
     end
+    url = url:gsub("\n", "\r\n")
+    url = url:gsub("([^%w ])", char_to_hex)
+    url = url:gsub(" ", "+")
+    return url
+end
 
-    local filtered = {};
-    for index, item in pairs(response) do
-        local url = (item["name"] .. "." .. item["tld"]);
-        if string.find(string.lower(url), string.lower(content)) then
-            table.insert(filtered, item);
+-- Arrays from the fetch function are converted to userdata, userdata is not iterable on it's own.
+-- Therefore we create our own iterable function.
+local function userdata_iter(ud)
+    local index = 0
+    local size = #ud
+    return function()
+        index = index + 1
+        if index <= size then
+            return index, ud[index]
         end
     end
+end
 
-    for index, item in pairs(filtered) do
+-- Fetch domains from the DNS using the given query.
+function fetchDomains(_query, _page)
+    local response = fetch({
+        url = dns .. "?search=" .. urlencode(_query) .. "&page=" .. _page .. "&limit=" .. limit,
+        method = "GET",
+        headers = { ["Content-Type"] = "application/json" }
+    });
+    return response["data"];
+end
+
+-- Fetch a random item from the dns.
+function fetchRandomItem()
+    local response = fetch({
+        url = dns .. "?search=",
+        method = "GET",
+        headers = { ["Content-Type"] = "application/json" }
+    });
+    print('a')
+    local allItems = response["data"];
+    print('a')
+
+    return allItems[math.random(#allItems)];
+end
+
+-- Clears all items from the results.
+function clearItems()
+    local nameEl = get('random-item-name');
+    nameEl.set_content('');
+
+    for index, item in pairs(items) do
+        --item.set_opacity(0);
         local itemEl = items[index];
         local nameEl = itemName[index];
         local ipEl = itemIp[index];
         local urlEl = itemUrl[index];
-
-        local url = "buss://" .. item["name"] .. "." .. item["tld"];
-
-        itemEl.set_opacity(1);
-        nameEl.set_content(item["name"]);
-        ipEl.set_content(item["ip"]);
-        urlEl.set_content(url);
-        urlEl.set_href(url);
+        nameEl.set_content('');
+        ipEl.set_content('');
+        urlEl.set_content('');
+        urlEl.set_href('');
     end
 end
 
-query.on_submit(queryServer);
+-- Displays the given item at the given index in the results.
+function displayItem(index, item)
+    local itemEl = items[index];
+    local nameEl = itemName[index];
+    local ipEl = itemIp[index];
+    local urlEl = itemUrl[index];
 
-btn.on_click(function()
-    queryServer(query.get_content())
-end)
-
-rndmbtn.on_click(function()
-    for index, item in pairs(items) do
-        item.set_opacity(0);
-    end
-
-    local field = items[1];
-    local itemEl = items[1];
-    local nameEl = itemName[1];
-    local ipEl = itemIp[1];
-    local urlEl = itemUrl[1];
-
-    field.set_opacity(1);
-
-    local item = response[math.random(#response)];
-    local url = "buss://" .. item["name"] .. "." .. item["tld"];
-
-    itemEl.set_opacity(1);
+    local url = "buss://" .. getURL(item);
     nameEl.set_content(item["name"]);
-    ipEl.set_content(item["ip"]);
+    nameEl.set_href(url);
+    ipEl.set_content(item["target"]);
+    urlEl.set_content(url);
+    urlEl.set_href(url);
+end
+
+-- Displays an array of items in the results.
+function displayItems(arr)
+    clearItems();
+    for index, item in userdata_iter(arr) do
+        displayItem(index, item);
+    end
+end
+
+function updatePages()
+    totalPage.set_content(totalPages);
+    currentPage.set_content(page + 1);
+    displayItems(fetchDomains(queried, page));
+end
+
+-- Retrieve the contents of the input and apply the query.
+function applyQuery()
+    queried = query.get_content();
+    page = 0;
+    updatePages();
+end
+
+function nextPage()
+    page = math.min(page + 1, totalPages);
+    updatePages();
+end
+
+function previousPage()
+    page = math.max(page - 1, 0);
+    updatePages();
+end
+
+-- Event Listeners
+--nextbtn.on_click(nextPage);
+--previousbtn.on_click(previousPage);
+query.on_submit(applyQuery);
+btn.on_click(applyQuery);
+rndmbtn.on_click(function()
+    clearItems();
+    local itemEl = get('random-item');
+    local nameEl = get('random-item-name');
+    local ipEl = get('random-item-ip');
+    local urlEl = get('random-item-url');
+
+    local item = fetchRandomItem();
+    local url = "buss://" .. getURL(item);
+    nameEl.set_content(item["name"]);
+    nameEl.set_href(url);
+    ipEl.set_content(item["target"]);
     urlEl.set_content(url);
     urlEl.set_href(url);
 end)
+
+-- Run main thread
+main();
